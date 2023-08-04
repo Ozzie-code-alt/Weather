263ee0f54a1d70086079a7f5aa1ff14ef2223922
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-05 00:14:49 +0800

    React States

--- weather/src/components/Weather.jsx
+++ weather/src/components/Weather.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+
+
+
+function Weather() {
+  const [value, setValue] = useState('')
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setValue(value);
+  };
+
+  
 const api_key = "f239f1ad70b1459f9e8141247230408"
-const Location = 'Paris'
+const Location = value
+
 const api_url =  `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${Location}`
 async function getApi(url){
   const response = await fetch(url)
@@ -11,13 +25,12 @@ async function getApi(url){
 
 getApi(api_url)
 
-
-function Weather() {
   return (
     <div>
         <label htmlFor="PlaceInput">Place: </label>
-        <input type="text" id='PlaceInput' name='PlaceInput'/>
+        <input type="text" value={value} name='PlaceInput' onChange={handleInputChange}/>
         <input type="submit" />
+        <p>{value}</p>
     </div>
   )
 }
